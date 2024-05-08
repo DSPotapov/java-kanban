fff860141679f478c9561ab67afb33cd6021b685
DSPotapov <p0tapovdenis@yandex.ru>
2024-05-08 20:23:24 +0300

    add Task subclasses

---
+++ src/Subtask.java
@@ -0,0 +1,5 @@
+public class Subtask extends Task{
+    public Subtask(String name, String description, int id) {
+        super(name, description, id);
+    }
+}
--- src/Task.java
+++ src/Task.java
@@ -1,2 +1,33 @@
 public class Task {
+    private final String name;
+    private final String description;
+    private final int id;
+    private TaskStatus taskStatus;
+
+    public Task(String name, String description, int id) {
+        this.name = name;
+        this.description = description;
+        this.id = id;
+        this.taskStatus = TaskStatus.NEW;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public TaskStatus getTaskStatus() {
+        return taskStatus;
+    }
+
+    public void setTaskStatus(TaskStatus taskStatus) {
+        this.taskStatus = taskStatus;
+    }
 }
