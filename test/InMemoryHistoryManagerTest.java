cb497e77dfed329a2a2213388c6b828b41ccfb71
DSPotapov <p0tapovdenis@yandex.ru>
2024-06-11 19:07:52 +0300

    update coding convertion

--- test/InMemoryHistoryManagerTest.java
+++ test/InMemoryHistoryManagerTest.java
@@ -13,42 +13,43 @@ class InMemoryHistoryManagerTest {
 //    private Task task = new Task("components.Task for test", "components.Task testing", 1);
 
     @BeforeEach
-    public void clearHistory(){
+    public void clearHistory() {
         historyManager.clearHistory();
     }
+
     @Test
     void addTest() {
         Task task = new Task("components.Task for test", "components.Task testing", 1);
         historyManager.add(task);
         final List<Task> history = historyManager.getHistory();
-        assertNotNull(history, "История не пустая.");
-        assertEquals(1, history.size(), "История не пустая.");
+        assertNotNull(history, "пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ.");
+        assertEquals(1, history.size(), "пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ.");
     }
 
     @Test
-    public void linkLastTest(){
+    public void linkLastTest() {
         Task task = new Task("components.Task for test", "components.Task testing", 1);
         Task task1 = new Task("components.Task1 for test", "components.Task1 testing", 2);
         historyManager.add(task);
         historyManager.add(task1);
-        assertEquals(historyManager.getLast(), task1, "последняя добавленая задача не совпадает с последней в истории");
+        assertEquals(historyManager.getLast(), task1, "пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ");
     }
 
     @Test
-    public void getFirstTest(){
+    public void getFirstTest() {
         Task task = new Task("components.Task for test", "components.Task testing", 1);
         Task task1 = new Task("components.Task1 for test", "components.Task1 testing", 2);
         historyManager.add(task);
         historyManager.add(task1);
-        assertEquals(historyManager.getFirst(), task, "первая добавленая задача не совпадает с первой в истории");
+        assertEquals(historyManager.getFirst(), task, "couldn't get head of history");
     }
 
     @Test
-    public void shouldNotBeDuplicatesInHistory(){
+    public void shouldNotBeDuplicatesInHistory() {
         Task task = new Task("task for updateTaskTest", "testing task for updateTaskTest");
         historyManager.add(task);
         historyManager.add(task);
-        assertEquals(1, historyManager.getHistory().size(), "найден дубликат в истории");
+        assertEquals(1, historyManager.getHistory().size(), "found duplicate in history");
     }
 
 }
