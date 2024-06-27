877f2ba89f1634205035998986668861c5432fbc
DSPotapov <p0tapovdenis@yandex.ru>
2024-06-27 21:26:52 +0300

    add taskToStrin, collectAllTasks  methods

--- src/managers/FileBackedTaskManager.java
+++ src/managers/FileBackedTaskManager.java
@@ -5,6 +5,10 @@ import components.SubTask;
 import components.Task;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
 public class FileBackedTaskManager extends InMemoryTaskManager {
     File file;
@@ -15,9 +19,28 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
 
     private void save() {
         // TODO соханять состояние менеджера в файл path
-        //1 Записываем таски
-        //2 Записываем эпики
-        //3 Записываем сабтаски
+        /*
+            файл каждый раз полностью перезаписывается
+            1. пишем строку заголовков
+            2. пишет таски, эпики, сабтаски
+            3. чтобы записать данные по порядку айдишников, нужно сначала
+            записать таски в Map<id, String> потом записать в файл
+            4. получить список keys из общей мапы тасок, отсортировать и
+            проходя по отсортированному списку вызывать соответствующий элемент мапы
+            и записывать в файл по порядку
+         */
+
+        Map<Integer, String> taskMap = collectAllTasks();
+        Set<Integer> ids = taskMap.keySet();
+
+        Arrays.sort(ids);
+
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write("id,type,name,status,description,epic\n");
+
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 
@@ -25,10 +48,45 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         return new FileBackedTaskManager(file);
     }
 
-    public String toString(Task task){
-        //TODO метод сохранения задачи в строку
+    public String taskToString(Task task){
+        String taskString = task.getId() + ", "
+                + task.getTaskType() + ", "
+                + task.getName() + ", "
+                + task.getTaskStatus() + ", "
+                + task.getDescription();
+        return taskString;
+    }
 
-        return "task";
+    public String subTaskToString(SubTask task){
+        String taskString = task.getId() + ", "
+                + task.getTaskType() + ", "
+                + task.getName() + ", "
+                + task.getTaskStatus() + ", "
+                + task.getDescription() + ", "
+                + task.getEpicId();
+        return taskString;
+    }
+
+    /**
+     * собираем таски сабтаски и эпики в одну общую мапу
+     * @return мап id таски : строковое представление таски
+     */
+    public Map<Integer, String> collectAllTasks(){
+        Map<Integer, String> taskMap = new HashMap<>();
+
+        for (Task task : tasks.values()){
+            taskMap.put(task.getId(), taskToString(task));
+        }
+
+        for (Epic task : epics.values()){
+            taskMap.put(task.getId(), taskToString(task));
+        }
+
+        for (SubTask task : subTasks.values()){
+            taskMap.put(task.getId(), subTaskToString(task));
+        }
+
+        return taskMap;
     }
 
     public Task fromString(String value){
